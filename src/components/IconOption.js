a541fac870836f362fd9765fab8a9397647ba8bc
Vedang Danej <vedangdanej34@gmail.com>
2021-04-28 19:04:51 +0530

    fixed width on tabs

--- src/components/IconOption.js
+++ src/components/IconOption.js
@@ -3,7 +3,7 @@ import tabRectangle from "../icons/tabRectangle.png";
 const IconOption = (props) => {
   return (
     <React.Fragment>
-      <div className="icon-option">
+      <div className={`icon-option ${props.img ? null : "increase-width"} `}>
         {props.img ? <img className="tab-icons" src={props.img}></img> : null}
         <h2 className={props.tabActive ? "tab-active" : null}>{props.name}</h2>
       </div>
